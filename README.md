be1f62a48f8f9a34cccbd62ea0989ae0c3e02f38
chenshy25 <1712823010@qq.com>
2025-10-22 23:59:34 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,33 @@
+# An Empirical Study of Vision Transformers for Fine-Grained Visual Classification
+
+**A Project for Artificial Neural Networks**
+
+## Report
+The detailed project report and slides can be found in the `report/` directory.
+
+
+## Setup
+
+For dependencies, pre-trained model downloads, and dataset preparation, please follow the instructions provided in the original [**TransFG GitHub repository**](https://github.com/TACJu/TransFG).
+
+## Training
+
+Once the setup is complete, use the following commands to start training.
+
+*   **Train on CUB-200-2011:**
+```bash
+python train.py \
+    --name CUB_ViT-B_16 \
+    --dataset cub \
+    --model_type ViT-B_16 \
+    --pretrained_dir /path/to/your/model/ViT-B_16.npz
+```
+
+*   **Train on Stanford Dogs:**
+```bash
+python train.py \
+    --name Dog_ViT-B_16 \
+    --dataset dog \
+    --model_type ViT-B_16 \
+    --pretrained_dir /path/to/your/model/ViT-B_16.npz
+```
---
+++ utils/parser.py
@@ -0,0 +1,67 @@
+import argparse
+
+def parse_args():
+     parser = argparse.ArgumentParser()
+     # Required parameters
+     parser.add_argument("--name", required=True,
+                         help="Name of this run. Used for monitoring.")
+     parser.add_argument("--dataset", choices=["CUB_200_2011", "car", "dog", "nabirds", "INat2017"], default="CUB_200_2011",
+                         help="Which dataset.")
+     parser.add_argument('--data_root', type=str, default='data/CUB_200_2011')
+     parser.add_argument("--model_type", choices=["ViT-B_16", "ViT-B_32", "ViT-L_16",
+                                                  "ViT-L_32", "ViT-H_14"],
+                         default="ViT-B_16",
+                         help="Which variant to use.")
+     parser.add_argument("--pretrained_dir", type=str, default="pretrained_models/ViT-B_16.npz",
+                         help="Where to search for pretrained ViT models.")
+     parser.add_argument("--pretrained_model", type=str, default=None,
+                         help="load pretrained model")
+     parser.add_argument("--output_dir", default="./output", type=str,
+                         help="The output directory where checkpoints will be written.")
+     parser.add_argument("--img_size", default=448, type=int,
+                         help="Resolution size")
+     parser.add_argument("--train_batch_size", default=16, type=int,
+                         help="Total batch size for training.")
+     parser.add_argument("--eval_batch_size", default=8, type=int,
+                         help="Total batch size for eval.")
+     parser.add_argument("--eval_every", default=100, type=int,
+                         help="Run prediction on validation set every so many steps."
+                              "Will always run one evaluation at the end of training.")
+
+     parser.add_argument("--learning_rate", default=3e-2, type=float,
+                         help="The initial learning rate for SGD.")
+     parser.add_argument("--weight_decay", default=0, type=float,
+                         help="Weight deay if we apply some.")
+     parser.add_argument("--num_steps", default=10000, type=int,
+                         help="Total number of training epochs to perform.")
+     parser.add_argument("--decay_type", choices=["cosine", "linear"], default="cosine",
+                         help="How to decay the learning rate.")
+     parser.add_argument("--warmup_steps", default=500, type=int,
+                         help="Step of training to perform learning rate warmup for.")
+     parser.add_argument("--max_grad_norm", default=1.0, type=float,
+                         help="Max gradient norm.")
+
+     parser.add_argument("--local_rank", type=int, default=-1,
+                         help="local_rank for distributed training on gpus")
+     parser.add_argument('--seed', type=int, default=42,
+                         help="random seed for initialization")
+     parser.add_argument('--gradient_accumulation_steps', type=int, default=1,
+                         help="Number of updates steps to accumulate before performing a backward/update pass.")
+     parser.add_argument('--fp16', action='store_true',
+                         help="Whether to use 16-bit float precision instead of 32-bit")
+     parser.add_argument('--fp16_opt_level', type=str, default='O2',
+                         help="For fp16: Apex AMP optimization level selected in ['O0', 'O1', 'O2', and 'O3']."
+                              "See details at https://nvidia.github.io/apex/amp.html")
+     parser.add_argument('--loss_scale', type=float, default=0,
+                         help="Loss scaling to improve fp16 numeric stability. Only used when fp16 set to True.\n"
+                              "0 (default value): dynamic loss scaling.\n"
+                              "Positive power of 2: static loss scaling value.\n")
+
+     parser.add_argument('--smoothing_value', type=float, default=0.0,
+                         help="Label smoothing value\n")
+
+     parser.add_argument('--split', type=str, default='non-overlap',
+                         help="Split method")
+     parser.add_argument('--slide_step', type=int, default=12,
+                         help="Slide step for overlap split")
+     return parser.parse_args()
